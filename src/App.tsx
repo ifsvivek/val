cc02f46a9540541de3e509b81c40e83744ffc02f
ifsvivek <vs63961@gmail.com>
2024-02-04 21:28:19 +0530

    sync

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,36 @@ const phrases = [
   "Don't do this to me",
   "I'm gonna cry...",
   "You're breaking my heart 🥹",
+  "Is there no love left?",
+  "Think about it one more time",
+  "It's not you, it's me",
+  "I thought we had something special",
+  "But what about our memories?",
+  "Just a friendly reminder, it's Valentine's Day",
+  "Love is a battlefield, but this feels like a war crime",
+  "Can we at least be friends?",
+  "You're like the cupid who forgot the arrows",
+  "This hurts more than a thousand heartbreak songs",
+  "Is this a bad dream? Pinch me!",
+  "Is there a 'Maybe' option?",
+  "Did someone replace your heart with a stone?",
+  "Why would you say that?",
+  "Not even a little love?",
+  "Is this a joke?",
+  "I thought we had something special",
+  "I'll be in my room listening to sad songs now",
+  "This hurts more than a breakup",
+  "You're cold-hearted",
+  "Do you even believe in love?",
+  "I guess love is not in the air",
+  "My heart just shattered",
+  "Well, there goes my Valentine's Day",
+  "I didn't see that coming",
+  "I feel like I've been stabbed in the heart",
+  "Is this the end of us?",
+  "Valentine's Day is canceled for me now",
 ];
+
 
 function App() {
   const [noCount, setNoCount] = useState(0);
@@ -30,7 +59,7 @@ function App() {
         <>
           <img
             alt="bears kissing"
-            src="https://media.tenor.com/D1CAg1rBD6wAAAAi/tkthao219-bubududu.gif"
+            src="https://media.tenor.com/ICmg8MUfSg8AAAAi/milk-and-mocha.gif"
           />
           <div className="text">Yay!!!!</div>
         </>
@@ -41,7 +70,7 @@ function App() {
             src="https://media.tenor.com/VIChDQ6ejRQAAAAi/jumping-bear-hearts-no-png.gif"
           />
 
-          <div>Will you be my Valentine?</div>
+          <div className="text">Will you be my Valentine?</div>
           <div>
             <button
               className="yes-button"
